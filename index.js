eb805390c89263c4618e18f169d25eee748810e4
MEHTA Meet <meet.mehta@soprasteria.com>
2020-08-30 18:50:54 +0530

    create schema and save data to mongo

--- index.js
+++ index.js
@@ -1,4 +1,25 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost')
-.then(()=>console.log('connected to the mongo db'))
-.catch(()=>console.log("Error connecting to the db"));
+mongoose.connect('mongodb://localhost/playground')
+    .then(() => console.log('connected to the mongo db'))
+    .catch(() => console.log("Error connecting to the db"));
+
+const courseSchema = new mongoose.Schema({
+    name: String,
+    author: String,
+    tags: [String],
+    date: { type: Date, default: Date.now },
+    isPublished: Boolean
+
+})
+const Course = mongoose.model('Course',courseSchema);
+async function createCourse(){
+const course = new Course ({
+    name : 'react.js',
+    author : "Meet Mehta",
+    tags : ["react.js","mongoDB","Express",'node.js'],
+    isPublished : true
+})
+const result = await course.save()
+console.log(result);
+}
+createCourse();
