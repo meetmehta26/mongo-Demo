d2b1730b2ce5eeaf06c1e3a9dfa7e72656cb2a9f
MEHTA Meet <meet7b@gmail.com>
2020-09-05 00:43:00 +0530

    MongoDb update by query Update directly

--- Updatecourse.js
+++ Updatecourse.js
@@ -19,21 +19,32 @@ async function inserToDB() {
     const course = new Courses({
         name: "NodJs",
         author: "Meet Mehta",
-        tags: ["NodeJs","backend","frontend"],
+        tags: ["NodeJs", "backend", "frontend"],
         isPublished: true,
         price: 15
     })
 
-const result = await course.save();
-console.log('Result',result);
+    const result = await course.save();
+    console.log('Result', result);
 }
-async function Updateone(id){
-    let response= await Courses.findById(id)
-    response.tags = ["high end","low end"];
+async function UpdateByQuery(id) {
+    let response = await Courses.findById(id)
+    response.tags = ["high end", "low end"];
 
     const result = await response.save()
     console.log(result);
 }
 
 
-Updateone('5f528ccf53e0d431a8983eff');
+// UpdateByQuery('5f528ccf53e0d431a8983eff');
+
+async function UpdateDirectly(id) {
+    result = await Courses.update({ _id: id }, {
+        name: "Angraj",
+        authord: "Mehta Meet"
+    })
+
+    console.log(result)
+}
+
+UpdateDirectly('5f528ccf53e0d431a8983eff');
